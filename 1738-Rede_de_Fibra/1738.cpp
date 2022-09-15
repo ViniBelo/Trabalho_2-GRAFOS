0a1069de6fde740348eca5fcc72f59d372cf610b
Vinicius Alves Beló <vinialvesbelo@outlook.com>
2022-09-15 20:20:39 -0300

    First commit

---
+++ 1738-Rede_de_Fibra/1738.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+char Floyd_Warshal(char *adjacencias, int a){
+    char D[a][a] = {};
+    for (int i = 1; i <= a; i++) {
+        for(int j = 1; j <= a; j++) {
+            for(int k = 1; k <= a; k++) {
+
+            }
+        }
+    }
+    return D;
+}
+
+main() {
+    int n, a, b;
+    string provedores;
+    
+    while ((cin >> n) && n > 0) {
+        char adjacencias[n + 1][n + 1];
+        cin >> a >> b >> provedores;
+        
+    }
+}
